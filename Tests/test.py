6c5c4dbdf55e2c83ff831d402a59d4759074bed2
Jagadeesh <vedulajagadeesh1994@gmail.com>
2026-02-15 16:28:55 +0530

    feat: Add new Playwright test, Gemini API key configuration, and report styling.

--- Tests/test.py
+++ Tests/test.py
@@ -11,9 +11,7 @@ def test_homepage_title(page,test_name):
     common.create_playwright_session("https://www.amazon.in/")
     common.test_has_title("Online Shopping")
     search_for_product(page,test_name)
-    print("Searching for wrong locator")
-    common.click_element('xpath=//input[@type="sub"]')
-    print("processed with Gen AI locator")
+    common.click_element('xpath=//input[@type="submit"]')
     common.test_has_title("shoes for man")
 
 def search_for_product(page,test_name):
